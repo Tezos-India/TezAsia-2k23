4e6b32c2bb8e03ac1a7f7bbf10845ad48186e357
Yadnyesh Mahadeshwar <yadnyesh.mahadeshwar@gmail.com>
2023-08-03 10:27:42 +0530

    MAINNET TO TESTNET

--- 695650-U2Q38ZH2/ui/dapp/tezos.ts
+++ 695650-U2Q38ZH2/ui/dapp/tezos.ts
@@ -1,17 +1,17 @@
-import { TezosToolkit } from '@taquito/taquito';
-import { BeaconWallet } from '@taquito/beacon-wallet';
-import { NetworkType } from '@airgap/beacon-sdk';
+import { TezosToolkit } from "@taquito/taquito";
+import { BeaconWallet } from "@taquito/beacon-wallet";
+import { NetworkType } from "@airgap/beacon-sdk";
 
-const Tezos = new TezosToolkit('https://testnet-tezos.giganode.io');
+const Tezos = new TezosToolkit("https://ghostnet.smartpy.io");
 
 const options = {
-  name: 'MyAwesomeDapp',
-  iconUrl: 'https://tezostaquito.io/img/favicon.svg',
-  preferredNetwork: NetworkType.MAINNET,
+  name: "MyAwesomeDapp",
+  iconUrl: "https://tezostaquito.io/img/favicon.svg",
+  preferredNetwork: NetworkType.GHOSTNET,
   eventHandlers: {
     PERMISSION_REQUEST_SUCCESS: {
       handler: async (data: any) => {
-        console.log('permission data:', data);
+        console.log("permission data:", data);
       },
     },
   },
@@ -22,7 +22,7 @@ const wallet = new BeaconWallet(options);
 Tezos.setWalletProvider(wallet);
 
 export async function connectWallet() {
-  await wallet.requestPermissions({ network: { type: NetworkType.MAINNET } });
+  await wallet.requestPermissions({ network: { type: NetworkType.GHOSTNET } });
   const userAddress = await wallet.getPKH();
   console.log(`Connected with ${userAddress}`);
 
