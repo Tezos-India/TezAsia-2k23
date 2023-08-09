093378dcc409accc8716eb57b6fa75e25c29006b
Yadnyesh Mahadeshwar <yadnyesh.mahadeshwar@gmail.com>
2023-08-09 10:24:43 +0530

    Updated navbar to directly handle wallet connection

--- 695650-U2Q38ZH2/ui/components/Navbar.tsx
+++ 695650-U2Q38ZH2/ui/components/Navbar.tsx
@@ -1,17 +1,53 @@
-import { useState } from "react";
-import { connectWallet } from "../dapp/tezos";
+import { useEffect, useState } from "react";
+import { connectWallet, disconnect, getAccount } from "../dapp/tezos";
 import Form from "./Form";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Navbar() {
+  const [account, setAccount] = useState<string>("");
+
+  useEffect(() => {
+    (async () => {
+      const account = await getAccount();
+      setAccount(account);
+    })();
+  }, []);
+
+  const onConnectWallet = async () => {
+    await connectWallet();
+    const account = await getAccount();
+    setAccount(account);
+  };
+
+  const onDisconnectWallet = () => {
+    disconnect();
+    setAccount("");
+  };
+
   return (
     <div className="flex justify-between items-center gap-8 ml-[80px] mr-8  pt-6 pb-6">
       <div className="text-white text-6xl font-sans">EtherStrike</div>
-      <Link rel="stylesheet" href="/Library" >
-      <button className="text-white text-3xl bg-blue-500 rounded-full h-12 px-8 font-['Roboto'] "  >Library</button>
+      <Link rel="stylesheet" href="/Library">
+        <button className="text-white text-3xl bg-blue-500 rounded-full h-12 px-8 font-['Roboto'] ">
+          Library
+        </button>
       </Link>
-      <Form />
+      {account ? (
+        <button
+          className="text-white text-3xl bg-blue-500 rounded-full h-12 px-8 font-['Roboto'] "
+          onClick={onDisconnectWallet}
+        >
+          {account}
+        </button>
+      ) : (
+        <button
+          className="text-white text-3xl bg-blue-500 rounded-full h-12 px-8 font-['Roboto'] "
+          onClick={onConnectWallet}
+        >
+          Connect Wallet
+        </button>
+      )}
+      {/* <Form /> */}
     </div>
   );
 }
-
