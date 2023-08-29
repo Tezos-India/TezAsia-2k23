1f1f0ac8ae7c0cbda86fa269088706b0efda7c4e
Saksham Bisen <sakshambisen123@gmail.com>
2023-08-29 11:15:20 +0530

    removed .env and added offers page

--- TezProj-02/app/src/CataloguePage.tsx
+++ TezProj-02/app/src/CataloguePage.tsx
@@ -1,28 +1,222 @@
-import { useMediaQuery } from "@mui/material";
+import { InfoOutlined } from "@mui/icons-material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  ImageList,
+  Pagination,
+  Tooltip,
+  useMediaQuery,
+} from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 
-import React from "react";
+import BigNumber from "bignumber.js";
+import { useFormik } from "formik";
+import { useSnackbar } from "notistack";
+import React, { Fragment, useState } from "react";
+import * as yup from "yup";
 import { UserContext, UserContextType } from "./App";
+import ConnectButton from "./ConnectWallet";
+import { TransactionInvalidBeaconError } from "./TransactionInvalidBeaconError";
+import { address, nat } from "./type-aliases";
+
+const itemPerPage: number = 6;
+
+type OfferEntry = [nat, Offer];
+
+type Offer = {
+  owner: address;
+  price: nat;
+};
+
+const validationSchema = yup.object({});
 
 export default function WineCataloguePage() {
-  const { nftContratTokenMetadataMap } = React.useContext(
-    UserContext
-  ) as UserContextType;
+  const {
+    Tezos,
+    nftContratTokenMetadataMap,
+    setUserAddress,
+    setUserBalance,
+    wallet,
+    userAddress,
+    nftContrat,
+    refreshUserContextOnPageReload,
+    storage,
+  } = React.useContext(UserContext) as UserContextType;
+  const [selectedOfferEntry, setSelectedOfferEntry] =
+    React.useState<OfferEntry | null>(null);
+
+  const formik = useFormik({
+    initialValues: {
+      quantity: 1,
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log("onSubmit: (values)", values, selectedOfferEntry);
+      buy(selectedOfferEntry!);
+    },
+  });
+  const { enqueueSnackbar } = useSnackbar();
+  const [currentPageIndex, setCurrentPageIndex] = useState<number>(1);
+
+  const buy = async (selectedOfferEntry: OfferEntry) => {
+    try {
+      const op = await nftContrat?.methods
+        .buy(
+          BigNumber(selectedOfferEntry[0]) as nat,
+          selectedOfferEntry[1].owner
+        )
+        .send({
+          amount: selectedOfferEntry[1].price.toNumber(),
+          mutez: true,
+        });
+
+      await op?.confirmation(2);
+
+      enqueueSnackbar(
+        "Bought " +
+          1 +
+          " unit of Wine collection (token_id:" +
+          selectedOfferEntry[0] +
+          ")",
+        {
+          variant: "success",
+        }
+      );
+
+      refreshUserContextOnPageReload(); //force all app to refresh the context
+    } catch (error) {
+      console.table(`Error: ${JSON.stringify(error, null, 2)}`);
+      let tibe: TransactionInvalidBeaconError =
+        new TransactionInvalidBeaconError(error);
+      enqueueSnackbar(tibe.data_message, {
+        variant: "error",
+        autoHideDuration: 10000,
+      });
+    }
+  };
   const isDesktop = useMediaQuery("(min-width:1100px)");
   const isTablet = useMediaQuery("(min-width:600px)");
-
   return (
     <Paper>
-      <Typography sx={{ paddingBottom: "10px" }} variant="h5">
-        Answer NFTs
+      <Typography style={{ paddingBottom: "10px" }} variant="h5">
+        Wine catalogue
       </Typography>
 
-      {nftContratTokenMetadataMap.size > 0 ? (
-        "//TODO"
+      {storage?.offers && storage?.offers.size != 0 ? (
+        <Fragment>
+          <Pagination
+            page={currentPageIndex}
+            onChange={(_, value) => setCurrentPageIndex(value)}
+            count={Math.ceil(
+              Array.from(storage?.offers.entries()).length / itemPerPage
+            )}
+            showFirstButton
+            showLastButton
+          />
+          <ImageList
+            cols={isDesktop ? itemPerPage / 2 : isTablet ? itemPerPage / 3 : 1}
+          >
+            {Array.from(storage?.offers.entries())
+
+              .filter((_, index) =>
+                index >= currentPageIndex * itemPerPage - itemPerPage &&
+                index < currentPageIndex * itemPerPage
+                  ? true
+                  : false
+              )
+              .map(([token_id, offer]) => (
+                <Card key={offer.owner + "-" + token_id.toString()}>
+                  <CardHeader
+                    avatar={
+                      <Tooltip
+                        title={
+                          <Box>
+                            <Typography>
+                              {" "}
+                              {"ID : " + token_id.toString()}{" "}
+                            </Typography>
+                            <Typography>
+                              {"Description : " +
+                                nftContratTokenMetadataMap.get(
+                                  token_id.toNumber()
+                                )?.description}
+                            </Typography>
+                            <Typography>
+                              {"Seller : " + offer.owner}{" "}
+                            </Typography>
+                          </Box>
+                        }
+                      >
+                        <InfoOutlined />
+                      </Tooltip>
+                    }
+                    title={
+                      nftContratTokenMetadataMap.get(token_id.toNumber())?.name
+                    }
+                  />
+                  <CardMedia
+                    sx={{ width: "auto", marginLeft: "33%" }}
+                    component="img"
+                    height="100px"
+                    image={nftContratTokenMetadataMap
+                      .get(token_id.toNumber())
+                      ?.thumbnailUri?.replace(
+                        "ipfs://",
+                        "https://gateway.pinata.cloud/ipfs/"
+                      )}
+                  />
+
+                  <CardContent>
+                    <Box>
+                      <Typography variant="body2">
+                        {" "}
+                        {"Price : " + offer.price.dividedBy(1000000) + " XTZ"}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+
+                  <CardActions>
+                    {!userAddress ? (
+                      <Box marginLeft="5vw">
+                        <ConnectButton
+                          Tezos={Tezos}
+                          nftContratTokenMetadataMap={
+                            nftContratTokenMetadataMap
+                          }
+                          setUserAddress={setUserAddress}
+                          setUserBalance={setUserBalance}
+                          wallet={wallet}
+                        />
+                      </Box>
+                    ) : (
+                      <form
+                        style={{ width: "100%" }}
+                        onSubmit={(values) => {
+                          setSelectedOfferEntry([token_id, offer]);
+                          formik.handleSubmit(values);
+                        }}
+                      >
+                        <Button type="submit" aria-label="add to favorites">
+                          <ShoppingCartIcon /> BUY
+                        </Button>
+                      </form>
+                    )}
+                  </CardActions>
+                </Card>
+              ))}
+          </ImageList>
+        </Fragment>
       ) : (
         <Typography sx={{ py: "2em" }} variant="h4">
-          Sorry, there is not NFT yet, you need to mint first
+          Sorry, there is not NFT to buy yet, you need to mint or sell bottles
+          first
         </Typography>
       )}
     </Paper>
--- TezProj-02/app/src/OffersPage.tsx
+++ TezProj-02/app/src/OffersPage.tsx
@@ -1,25 +1,310 @@
-import { Typography, useMediaQuery } from "@mui/material";
+import { InfoOutlined } from "@mui/icons-material";
+import SellIcon from "@mui/icons-material/Sell";
+
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  ImageList,
+  InputAdornment,
+  Pagination,
+  TextField,
+  Tooltip,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import Paper from "@mui/material/Paper";
-import React, { Fragment } from "react";
+import BigNumber from "bignumber.js";
+import { useFormik } from "formik";
+import { useSnackbar } from "notistack";
+import React, { Fragment, useEffect, useState } from "react";
+import * as yup from "yup";
 import { UserContext, UserContextType } from "./App";
+import ConnectButton from "./ConnectWallet";
+import { TransactionInvalidBeaconError } from "./TransactionInvalidBeaconError";
+import { address, nat } from "./type-aliases";
+
+const itemPerPage: number = 6;
+
+const validationSchema = yup.object({
+  price: yup
+    .number()
+    .required("Price is required")
+    .positive("ERROR: The number must be greater than 0!"),
+});
+
+type Offer = {
+  owner: address;
+  price: nat;
+};
 
 export default function OffersPage() {
-  const { nftContratTokenMetadataMap } = React.useContext(
-    UserContext
-  ) as UserContextType;
+  const [selectedTokenId, setSelectedTokenId] = React.useState<number>(0);
+  const [currentPageIndex, setCurrentPageIndex] = useState<number>(1);
+
+  let [offersTokenIDMap, setOffersTokenIDMap] = React.useState<Map<nat, Offer>>(
+    new Map()
+  );
+  let [ownerTokenIds, setOwnerTokenIds] = React.useState<Set<nat>>(new Set());
+
+  const {
+    nftContrat,
+    nftContratTokenMetadataMap,
+    userAddress,
+    storage,
+    refreshUserContextOnPageReload,
+    Tezos,
+    setUserAddress,
+    setUserBalance,
+    wallet,
+  } = React.useContext(UserContext) as UserContextType;
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const formik = useFormik({
+    initialValues: {
+      price: 0,
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log("onSubmit: (values)", values, selectedTokenId);
+      sell(selectedTokenId, values.price);
+    },
+  });
+
+  const initPage = async () => {
+    if (storage) {
+      console.log("context is not empty, init page now");
+      ownerTokenIds = new Set();
+      offersTokenIDMap = new Map();
+
+      await Promise.all(
+        storage.token_ids.map(async (token_id) => {
+          let owner = await storage.ledger.get(token_id);
+          if (owner === userAddress) {
+            ownerTokenIds.add(token_id);
+
+            const ownerOffers = await storage.offers.get(token_id);
+            if (ownerOffers) offersTokenIDMap.set(token_id, ownerOffers);
+
+            console.log(
+              "found for " +
+                owner +
+                " on token_id " +
+                token_id +
+                " with balance " +
+                1
+            );
+          } else {
+            console.log("skip to next token id");
+          }
+        })
+      );
+      setOwnerTokenIds(new Set(ownerTokenIds)); //force refresh
+      setOffersTokenIDMap(new Map(offersTokenIDMap)); //force refresh
+    } else {
+      console.log("context is empty, wait for parent and retry ...");
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      console.log("after a storage changed");
+      await initPage();
+    })();
+  }, [storage]);
+
+  useEffect(() => {
+    (async () => {
+      console.log("on Page init");
+      await initPage();
+    })();
+  }, []);
+
+  const sell = async (token_id: number, price: number) => {
+    try {
+      const op = await nftContrat?.methods
+        .sell(
+          BigNumber(token_id) as nat,
+          BigNumber(price * 1000000) as nat //to mutez
+        )
+        .send();
+
+      await op?.confirmation(2);
+
+      enqueueSnackbar(
+        "Wine collection (token_id=" +
+          token_id +
+          ") offer for " +
+          1 +
+          " units at price of " +
+          price +
+          " XTZ",
+        { variant: "success" }
+      );
+
+      refreshUserContextOnPageReload(); //force all app to refresh the context
+    } catch (error) {
+      console.table(`Error: ${JSON.stringify(error, null, 2)}`);
+      let tibe: TransactionInvalidBeaconError =
+        new TransactionInvalidBeaconError(error);
+      enqueueSnackbar(tibe.data_message, {
+        variant: "error",
+        autoHideDuration: 10000,
+      });
+    }
+  };
+
   const isDesktop = useMediaQuery("(min-width:1100px)");
   const isTablet = useMediaQuery("(min-width:600px)");
 
   return (
     <Paper>
-      <Typography sx={{ paddingBottom: "10px" }} variant="h5">
-        Sell AnswerNFTs
+      <Typography style={{ paddingBottom: "10px" }} variant="h5">
+        Sell my bottles
       </Typography>
-      {nftContratTokenMetadataMap.size > 0 ? (
-        <Fragment>//TODO</Fragment>
+      {ownerTokenIds && ownerTokenIds.size != 0 ? (
+        <Fragment>
+          <Pagination
+            page={currentPageIndex}
+            onChange={(_, value) => setCurrentPageIndex(value)}
+            count={Math.ceil(
+              Array.from(ownerTokenIds.entries()).length / itemPerPage
+            )}
+            showFirstButton
+            showLastButton
+          />
+
+          <ImageList
+            cols={isDesktop ? itemPerPage / 2 : isTablet ? itemPerPage / 3 : 1}
+          >
+            {Array.from(ownerTokenIds.entries())
+              .filter((_, index) =>
+                index >= currentPageIndex * itemPerPage - itemPerPage &&
+                index < currentPageIndex * itemPerPage
+                  ? true
+                  : false
+              )
+              .map(([token_id]) => (
+                <Card key={token_id + "-" + token_id.toString()}>
+                  <CardHeader
+                    avatar={
+                      <Tooltip
+                        title={
+                          <Box>
+                            <Typography>
+                              {" "}
+                              {"ID : " + token_id.toString()}{" "}
+                            </Typography>
+                            <Typography>
+                              {"Description : " +
+                                nftContratTokenMetadataMap.get(
+                                  token_id.toNumber()
+                                )?.description}
+                            </Typography>
+                          </Box>
+                        }
+                      >
+                        <InfoOutlined />
+                      </Tooltip>
+                    }
+                    title={
+                      nftContratTokenMetadataMap.get(token_id.toNumber())?.name
+                    }
+                  />
+                  <CardMedia
+                    sx={{ width: "auto", marginLeft: "33%" }}
+                    component="img"
+                    height="100px"
+                    image={nftContratTokenMetadataMap
+                      .get(token_id.toNumber())
+                      ?.thumbnailUri?.replace(
+                        "ipfs://",
+                        "https://gateway.pinata.cloud/ipfs/"
+                      )}
+                  />
+
+                  <CardContent>
+                    <Box>
+                      <Typography variant="body2">
+                        {offersTokenIDMap.get(token_id)
+                          ? "Traded : " +
+                            1 +
+                            " (price : " +
+                            offersTokenIDMap
+                              .get(token_id)
+                              ?.price.dividedBy(1000000) +
+                            " Tz)"
+                          : ""}
+                      </Typography>
+                    </Box>
+                  </CardContent>
+
+                  <CardActions>
+                    {!userAddress ? (
+                      <Box marginLeft="5vw">
+                        <ConnectButton
+                          Tezos={Tezos}
+                          nftContratTokenMetadataMap={
+                            nftContratTokenMetadataMap
+                          }
+                          setUserAddress={setUserAddress}
+                          setUserBalance={setUserBalance}
+                          wallet={wallet}
+                        />
+                      </Box>
+                    ) : (
+                      <form
+                        style={{ width: "100%" }}
+                        onSubmit={(values) => {
+                          setSelectedTokenId(token_id.toNumber());
+                          formik.handleSubmit(values);
+                        }}
+                      >
+                        <span>
+                          <TextField
+                            type="number"
+                            name="price"
+                            label="price"
+                            placeholder="Enter a price"
+                            variant="filled"
+                            value={formik.values.price}
+                            onChange={formik.handleChange}
+                            error={
+                              formik.touched.price &&
+                              Boolean(formik.errors.price)
+                            }
+                            helperText={
+                              formik.touched.price && formik.errors.price
+                            }
+                            InputProps={{
+                              endAdornment: (
+                                <InputAdornment position="end">
+                                  <Button
+                                    type="submit"
+                                    aria-label="add to favorites"
+                                  >
+                                    <SellIcon /> Sell
+                                  </Button>
+                                </InputAdornment>
+                              ),
+                            }}
+                          />
+                        </span>
+                      </form>
+                    )}
+                  </CardActions>
+                </Card>
+              ))}{" "}
+          </ImageList>
+        </Fragment>
       ) : (
         <Typography sx={{ py: "2em" }} variant="h4">
-          Sorry, there is not NFT yet, you need to mint bottles first
+          Sorry, you don't own any bottles, buy or mint some first
         </Typography>
       )}
     </Paper>
