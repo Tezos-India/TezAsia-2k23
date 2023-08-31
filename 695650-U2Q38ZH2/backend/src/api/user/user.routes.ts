7008780466eb544ebe9421da5521f962e1a2b544
Dharmendra solanki <78801686+ProgrammingPirates@users.noreply.github.com>
2023-08-31 22:03:24 +0530

    Delete

--- 695650-U2Q38ZH2/backend/src/api/user/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as UserController from "./user.controller";
-
-const router = express.Router();
-
-router.post("/user/upsert", UserController.upsertUser);
-router.get("/user/:walletAddress", UserController.getUserByWallet);
-
-export default router;
