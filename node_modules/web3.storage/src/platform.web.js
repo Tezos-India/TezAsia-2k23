fe3cb6e7286779c725bc26dabea00313b6ad9904
Ankit Choudhary <sign.ankit14@gmail.com>
2023-09-01 17:44:30 +0530

    Removed unnecessary files

--- node_modules/web3.storage/src/platform.web.js
+++
@@ -1,9 +0,0 @@
-// TODO: Use indexedDb
-import { MemoryBlockStore } from 'ipfs-car/blockstore/memory'
-
-export const fetch = globalThis.fetch
-export const Request = globalThis.Request
-export const Response = globalThis.Response
-export const Blob = globalThis.Blob
-export const File = globalThis.File
-export const Blockstore = MemoryBlockStore
